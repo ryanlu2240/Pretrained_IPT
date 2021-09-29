cd445135eb5204db8af3f590b6c7ad50e6015799
ryanlu2240 <kevinlu2240@gmail.com>
2021-09-29 21:45:47 +0800

    delete MyConcat Dataset class

--- data/__init__.py
+++ data/__init__.py
@@ -7,33 +7,25 @@ from importlib import import_module
 from torch.utils.data import dataloader
 from torch.utils.data import ConcatDataset
 
-# This is a simple wrapper function for ConcatDataset
-class MyConcatDataset(ConcatDataset):
-    def __init__(self, datasets):
-        super(MyConcatDataset, self).__init__(datasets)
-        self.train = datasets[0].train
-
-    def set_scale(self, idx_scale):
-        for d in self.datasets:
-            if hasattr(d, 'set_scale'): d.set_scale(idx_scale)
 
 class Data:
     def __init__(self, args):
         self.loader_train = []
         if not args.test_only:
-            datasets = []
             for d in args.data_train:
                 module_name = d if d.find('DIV2K-Q') < 0 else 'DIV2KJPEG'
                 m = import_module('data.' + module_name.lower())
-                datasets.append(getattr(m, module_name)(args, name=d))
+                trainset = (getattr(m, module_name)(args, name=d))
 
-            self.loader_train.append(dataloader.DataLoader(
-                MyConcatDataset(datasets),
-                batch_size=args.batch_size,
-                shuffle=True,
-                pin_memory=not args.cpu,
-                num_workers=args.n_threads,
-            ))
+                self.loader_train.append(
+                    dataloader.DataLoader(
+                        trainset,
+                        batch_size=args.batch_size,
+                        shuffle=True,
+                        pin_memory=not args.cpu,
+                        num_workers=args.n_threads,
+                    )
+                )
 
         self.loader_test = []
         for d in args.data_test:
