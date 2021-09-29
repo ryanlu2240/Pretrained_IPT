33cdc942194f60236848725f35042d892ae8fd2d
ryanlu2240 <kevinlu2240@gmail.com>
2021-09-29 21:46:41 +0800

    updata script

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0,4 python3 main.py --dir_data /home/shlu/dataset/ipt_data \
+CUDA_VISIBLE_DEVICES=3 python3 main.py --dir_data /home/shlu/dataset/ipt_data \
     --data_test Set5+Set14+B100+Urban100 \
-    --scale 3 --num_queries 1 --n_GPUs 2 --test_only --pretrain /home/shlu/IPT_model/model_2.pt
+    --scale 2+3+4 --num_queries 3 --n_GPUs 1 --test_only --pretrain /home/shlu/t/model/model_best.pt
